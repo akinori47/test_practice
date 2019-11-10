c01f7776b4fc2c0122f7f20763377df1813abfd4
Roman_Khafizov <hafaml@Mail.ru>
2019-11-10 23:21:46 +0500

    Initial commit

---
+++ task.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import hashlib
+import sys
+
+path_to_file = sys.argv[1]
+path_to_dir = sys.argv[2]
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
