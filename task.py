311cc85540feff5ea0d87fdd00dd7ac771bade5d
Roman_Khafizov <hafaml@Mail.ru>
2019-11-17 13:34:41 +0500

    complete third task

--- task.py
+++ task.py
@@ -3,12 +3,16 @@
 
 import hashlib
 import sys
+import os
 
 path_to_file = sys.argv[1]
 path_to_dir = sys.argv[2]
-#TODO: Описание к функциям
+
 
 def hasher(hash_alg: str, file):
+    """
+    Function return hash of the input file for a given algorithm
+    """
     hash_alg.lower()
     try:
         with open(file, 'rb') as a_file:
@@ -27,14 +31,15 @@ def hasher(hash_alg: str, file):
 def main():
     with open(path_to_file, "r") as source_file:
         for line in source_file:
-            result = line.split(" ", maxsplit=2)
-            hash_result = hasher(result[1], path_to_dir + r'\{}'.format(result[0])) #TODO Форматирование строки для OS
-            if hash_result == result[2].strip():
-                print(result[0] + " OK")
+            item = line.split(" ", maxsplit=2)
+            path = os.path.normpath(path_to_dir + r'\{}'.format(item[0]))
+            hash_result = hasher(item[1], path)
+            if hash_result == item[2].strip():
+                print(item[0] + " OK")
             elif hash_result == "NOT FOUND":
-                print(result[0] + " NOT FOUND")
+                print(item[0] + " NOT FOUND")
             else:
-                print(result[0] + " FAIL")
+                print(item[0] + " FAIL")
 
 
 if __name__ == "__main__":
