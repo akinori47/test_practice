60da88debd5ceaf113c5019d77f4407e8636554a
Roman_Khafizov <hafaml@Mail.ru>
2019-11-24 00:28:20 +0500

    edit third task rename third task file

--- hash.py
+++ hash.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+"""
+# Для запуска скрипта используйте
+# hash.py <путь к файлу> <путь к директории, содержащей файлы>
+"""
+
 import hashlib
 import sys
 import os
 
-path_to_file = sys.argv[1]
-path_to_dir = sys.argv[2]
+PATH_TO_FILE = sys.argv[1]
+PATH_TO_DIR = sys.argv[2]
 
 
 def hasher(hash_alg: str, file):
@@ -29,10 +34,10 @@ def hasher(hash_alg: str, file):
 
 
 def main():
-    with open(path_to_file, "r") as source_file:
+    with open(PATH_TO_FILE, "r") as source_file:
         for line in source_file:
             item = line.split(" ", maxsplit=2)
-            path = os.path.normpath(path_to_dir + r'\{}'.format(item[0]))
+            path = os.path.join(PATH_TO_DIR, item[0])
             hash_result = hasher(item[1], path)
             if hash_result == item[2].strip():
                 print(item[0] + " OK")
