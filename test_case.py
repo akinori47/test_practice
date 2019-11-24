4bbce208e996c58da0982b9794f2919218ff4f3c
Roman_Khafizov <hafaml@Mail.ru>
2019-11-24 19:22:16 +0500

    add requirements.txt resolve third task update gitignore

--- test_case.py
+++ test_case.py
@@ -1,31 +1,86 @@
-from unittest import TestCase
-import pytest
 import os
 import psutil
-
-# Вывод файлов домашнего каталога Linux
-for i in os.listdir(os.getenv('HOME')):
-    print(i)
-
-# Вывод кол-ва оперативной памяти
-ram = psutil.virtual_memory().total
-int_ram = int(ram * 10 ** -9)
+import time
+import sys
 
 
-# Команда для создания файла размером 1мб со случайным содержимым
-os.system('head -c 1MB /dev/urandom > /tmp/test')
-# Удаление файла
-os.remove("/tmp/test")
-
-class FileList(TestCase):
+class ListOfFiles(object):
     next_id = 0
 
     def __init__(self):
-        self.id = FileList.nextid
-        TestCase.nextid += 1
+        self.id = ListOfFiles.next_id
+        ListOfFiles.next_id += 1
         self.name = "Список файлов"
 
-    def setUp(self):
+    def prep(self):
+        if not int(time.time()) % 2 == 0:
+            print("time since the beginning of the Unix epoch not a multiple of two")
+            print("Test {} BLOCKED".format(self.name))
+            sys.exit()
+
+    @staticmethod
+    def run():
+        for file in os.listdir(os.getenv('HOME')):
+            print(file)
+
+    def clean_up(self):
         pass
 
+    def execute(self):
+        try:
+            print('Preparing for a test {}'.format(self.name))
+            self.prep()
+            print('Running test {}'.format(self.name))
+            self.run()
+            print("Cleaning UP test {}".format(self.name))
+            self.clean_up()
+            print("Test {} PASSED".format(self.name))
+        except OSError as error:
+            print(error)
+            print('Test {} FAILED'.format(self.name))
 
+
+class RandomFile(object):
+    next_id = 0
+
+    def __init__(self):
+        self.id = RandomFile.next_id
+        RandomFile.next_id += 1
+        self.name = "Cлучайный Файл"
+
+    def prep(self):
+        print('Preparing for a test')
+        ram = psutil.virtual_memory().total
+        ram = ram * 10 ** -9
+        if ram < 1:
+            print("Current RAM is lower that 1GB")
+            print("{} step prep - BLOCKED".format(self.name))
+            exit()
+        else:
+            pass
+
+    @staticmethod
+    def run():
+        os.system('head -c 1MB /dev/urandom > /tmp/test')
+
+    @staticmethod
+    def clean_up():
+        os.remove("/tmp/test")
+
+    def execute(self):
+        try:
+            print('Preparing for a test {}'.format(self.name))
+            self.prep()
+            print('Running test {}'.format(self.name))
+            self.run()
+            print("Cleaning UP test {}".format(self.name))
+            self.clean_up()
+            print("Test {} PASSED".format(self.name))
+        except OSError as error:
+            print(error)
+            print('Test {} FAILED'.format(self.name))
+
+
+if __name__ == "__main":
+    ListOfFiles.execute()
+    RandomFile.execute()
